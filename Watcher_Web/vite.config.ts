9ace6e7148f7a879dd6e16221bd5ef0b114910c0
juck89724 <juck89724@gmail.com>
2026-01-17 23:50:14 +0800

    build: Regenerate project build artifacts and reinstall web dependencies.

--- Watcher_Web/vite.config.ts
+++ Watcher_Web/vite.config.ts
@@ -4,7 +4,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FF14_Watcher/',
+  base: './',
   plugins: [
     react(),
     viteStaticCopy({
